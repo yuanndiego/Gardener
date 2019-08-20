720cac86f227d754e93c93d489ea1b80841ec29d
Chris Hurt <chrishcoding@gmail.com>
2019-08-21 09:39:48 +1000

    Updated schema, tasks now use DATE instead of TIMESTAMP for due_date

--- schema.sql
+++ schema.sql
@@ -2,10 +2,13 @@
 
 -- Copy & Paste this content --
 
+-- Copy Step 1 --
 CREATE DATABASE gardener_db;
 
+-- Copy Step 2 --
 \c gardener_db;
 
+-- Confirm connection & copy Step 3 --
 CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(300),
@@ -25,7 +28,7 @@ CREATE TABLE Tasks (
     plant_id INTEGER,
     name VARCHAR(300),
     description VARCHAR(400),
-    due_date TIMESTAMP
+    due_date DATE
 );
 
 CREATE TABLE Todos (
