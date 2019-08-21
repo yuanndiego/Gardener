c5d603aea6cdc3033fa21bedf6f6da80027dfd12
Chris Hurt <chrishcoding@gmail.com>
2019-08-21 10:29:09 +1000

    Added sinatra reloader to gemfile

--- main.rb
+++ main.rb
@@ -1,32 +1,9 @@
      
 require 'sinatra'
-require 'sinatra/reloader'
-also_reload File.expand_path(__dir__, 'models/*')
-also_reload File.expand_path(__dir__, 'views/*')
-also_reload File.expand_path(__dir__, 'routes/*')
-
-require 'bcrypt'
-require_relative 'database_config'
-
-enable :sessions
-
-helpers do
-  def loggedIn?
-    !!current_user
-  end
-
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
-end
 
 get '/' do
   erb :index
 end
-
-require_relative 'routes/plants'
-require_relative 'routes/todo'
-require_relative 'routes/login'
 
 
 
