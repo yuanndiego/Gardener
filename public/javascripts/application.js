9adc658afe40c378a9c4bb67f8cfafe5ddf1c6f5
Yu-Ann <yuann.chen@gmail.com>
2019-08-22 20:08:58 +1000

    add and my plant update: plant info hover over and CSS changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,12 +5,12 @@ const selectPlant = function(event) {
     if(event.currentTarget.firstElementChild.name === "") {
         event.target.closest('.plant-ui-card-container').querySelector('input').name = "plantSelected[]"
         event.target.classList.add("plant_selected_opacity")
-        event.target.closest('.plant-ui-card-container').querySelector('.add_plants_icons').src = '/icon_assets/baseline-check_circle_outline-24px.svg'
+        event.target.closest('.plant-ui-card-container').querySelector('.add_plants_icons').src = '/icon_assets/baseline-check_circle-24px.svg'
     } else {
         console.log("else statemebnt");
         event.target.closest('.plant-ui-card-container').querySelector('input').name = ""
         event.target.classList.remove("plant_selected_opacity")
-        event.target.closest('.plant-ui-card-container').querySelector('.add_plants_icons').src = '/icon_assets/baseline-add_circle_outline-24px.svg'
+        event.target.closest('.plant-ui-card-container').querySelector('.add_plants_icons').src = '/icon_assets/baseline-add_circle-24px.svg'
     }
 } 
 
