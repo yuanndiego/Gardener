b3772746771e869f4cd906d23df0c265ab518241
Chris Hurt <chrishcoding@gmail.com>
2019-08-23 15:51:48 +1000

    login form visibility bug fixed

--- public/javascripts/loginregister.js
+++ public/javascripts/loginregister.js
@@ -1,47 +1,44 @@
 /*Title: Cool Modal Popup Sign In/Out Form*/
 
 $(function() {
-    //defining all needed variables
-    var $overlay = $('.overlay');
-    var $mainPopUp = $('.main-popup')
-    var $signIn = $('#sign-in');
-    var $register = $('#register');
-    var $formSignIn = $('form.sign-in');
-    var $formRegister = $('form.register');
-    
-    $('button').on('click', function(){
-      $overlay.addClass('visible');
-      $mainPopUp.addClass('visible');
-      $signIn.addClass('active');
-      $register.removeClass('active');
-      $formRegister.removeClass('move-left');
-      $formSignIn.removeClass('move-left');
-    });
-    $overlay.on('click', function(){
-      $(this).removeClass('visible');
-      $mainPopUp.removeClass('visible');
-    });
-    $('#popup-close-button a').on('click', function(e){
-      e.preventDefault();
-      $overlay.removeClass('visible');
-      $mainPopUp.removeClass('visible');
-    });
-    
-    $signIn.on('click', function(){
-      $signIn.addClass('active');
-      $register.removeClass('active');
-      $formSignIn.removeClass('move-left');
-      $formRegister.removeClass('move-left');
-    });
-    
-    $register.on('click', function(){
-      $signIn.removeClass('active');
-      $register.addClass('active');
-      $formSignIn.addClass('move-left');
-      $formRegister.addClass('move-left');
-    });
-    
-    
+  //defining all needed variables
+  var $overlay = $('.overlay');
+  var $mainPopUp = $('.main-popup');
+  var $signIn = $('#sign-in');
+  var $register = $('#register');
+  var $formSignIn = $('form.sign-in');
+  var $formRegister = $('form.register');
+  
+  $('button').on('click', function(){
+    $overlay.addClass('visible');
+    $mainPopUp.addClass('visible');
+    $signIn.addClass('active');
+    $register.removeClass('active');
+    $formRegister.removeClass('move-left');
+    $formSignIn.removeClass('move-left');
+  });
+  $overlay.on('click', function(){
+    $(this).removeClass('visible');
+    $mainPopUp.removeClass('visible');
+  });
+  $('#popup-close-button a').on('click', function(e){
+    e.preventDefault();
+    $overlay.removeClass('visible');
+    $mainPopUp.removeClass('visible');
   });
 
   
+  $signIn.on('click', function(){
+    $signIn.addClass('active');
+    $register.removeClass('active');
+    $formSignIn.removeClass('move-left');
+    $formRegister.removeClass('move-left');
+  });
+  
+  $register.on('click', function(){
+    $signIn.removeClass('active');
+    $register.addClass('active');
+    $formSignIn.addClass('move-left');
+    $formRegister.addClass('move-left');
+  });
+});
