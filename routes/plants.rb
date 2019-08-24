7a53ed9682c5a1f70d73760beed6df92f986470b
Chris Hurt <chrishcoding@gmail.com>
2019-08-24 16:13:29 +1000

    404 error added, routing now references current user id

--- routes/plants.rb
+++ routes/plants.rb
@@ -1,25 +1,30 @@
 require 'pry'
 
 get '/my_plants' do 
-    # @plants = Plant.where(user_id: current_user)
+    redirect '/login' unless logged_in?
     @plants = Plant.all
     session[:url] = 'my_plants'
     erb :my_plants
 end
 
 get '/plants/new' do
+    redirect '/login' unless logged_in?
     @plants = Plant.all
     erb :add_plants
 end
 
 post '/my_plants' do
+    redirect '/login' unless logged_in?
     plants_selected= params["plantSelected"]
     plants_id_array = plants_selected.map(&:to_i)
     plants_id_array.each do |plantid|
         tasks = Task.where(plant_id: plantid)
         tasks.each do |task|
             todo = Todo.new
-            todo.user_id = User.first.id
+            
+            # todo.user_id = User.first.id
+            todo.user_id = current_user.id
+
             # change to current user once complete
             todo.plant_id = task.plant_id
             todo.task_id = task.id
@@ -31,6 +36,7 @@ post '/my_plants' do
 end
 
 # delete '/my_plants' do
+#     redirect '/login' unless logged_in?
 #     plant = plant.find(params[:id])
 #     plant.delete
 #     redirect '/my_plants'
